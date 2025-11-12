7aa9ffcbfc11bf9acac7cf4461631ec9de31bb08
Marcelo-Med <marcelo.medina.canto018@gmail.com>
2025-11-11 18:32:27 -0600

    Copletar Ejercicio 3.3: Tabla de multiplicar

--- ejercicios.js
+++ ejercicios.js
@@ -169,13 +169,18 @@ function sumaHastaN(n) {
 
 /**
  * Ejercicio 3.3: Tabla de multiplicar (5 puntos)
- * @param {number} numero - NÃºmero para generar su tabla
+ * @param {number} numero - Número para generar su tabla
  * @returns {Array} - Array con los resultados de numero x 1 hasta numero x 10
  * Ejemplo: tablaMultiplicar(3) debe retornar [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
  */
 function tablaMultiplicar(numero) {
-  // TODO: Genera un array con la tabla de multiplicar
+  // Genera un array con la tabla de multiplicar
   const tabla = [];
+
+  for (let i = 1; i <= 10; i++) {
+    tabla.push(numero * i);
+  }
+
   return tabla;
 }
 
